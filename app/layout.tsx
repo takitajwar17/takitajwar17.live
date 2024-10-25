150c445e1d4ece8ace40078f623e3e48b5f8d4bf
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-10-26 04:29:11 +0600

    data changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "TakiTajwar17.live",
+  description: "Taki Tajwaruzzaman Khan's Portfolio",
 };
 
 export default function RootLayout({
